44c3e9c778116c78b83d87941a963cf877a0f7ba
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-03-20 12:23:29 +0200

    :zap: create darkmodebtn

--- frontend/src/pages/Pages.tsx
+++ frontend/src/pages/Pages.tsx
@@ -1,18 +1,18 @@
-import type { FC } from 'react';
-import Login from './Login/Login';
+import type { FC } from "react";
+import Login from "./Login/Login";
+import DarkModeButton from "../components/DarkModeButton/DarckModeButton";
 
-interface PagesProps {
-
-}
+interface PagesProps {}
 
 const Pages: FC<PagesProps> = () => {
     return (
-        <div className='text-light-colors-text dark:text-dark-colors-text'>
-            <div className="h-screen w-screen">
-                <Login/>
+        <div className="text-light-colors-text dark:text-dark-colors-text">
+            <div className="h-screen w-screen  dark:bg-pr">
+                <Login />
             </div>
+            <DarkModeButton/>
         </div>
     );
-}
+};
 
 export default Pages;
