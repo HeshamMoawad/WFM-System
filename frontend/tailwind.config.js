77bad17e42c4f868d041e8c5fd0d9f5e29d50649
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-03-20 11:43:48 +0200

    :tada: init create-react-app

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    colors:{
+      'dark-colors':{
+        "login":{
+          "primary-bg":"#3b5d7a" ,
+          "secondry-bg":"#1f2f3e",
+          "third-bg":"#15202b",
+        },
+        "dashboard":{
+          "primary-bg":"#15202b" ,
+          "secondry-bg":"#1f2f3e",
+          "third-bg":"#22424f",
+        },
+        "text":"white"
+      },
+      'light-colors':{
+        "login":{
+          "primary-bg":"#f3fcfb" ,
+          "secondry-bg":"#ffffff",
+          "third-bg":"#f3fbfb",
+          },
+        "dashboard":{
+          "primary-bg":"#f8f8f8" ,
+          "secondry-bg":"#ffffff",
+          "third-bg":"#e6f7f7",
+          "forth-bg":"#1f2f3e"
+        },
+        "text":"black"
+      },
+      "primary":"#51c9c9",
+
+
+
+      
+      "pr-dark":'#1f2f3e' ,
+      "sec-dark" : '#35bfbf',
+      "pr-dark-2":"#3b5d7a" ,
+      "third-dark" : "#15202b" ,
+    } ,
+    extend: {},
+  },
+  plugins: [],
+}
+
