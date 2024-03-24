01ad1dffd9d7409599a2ad7859deda8f2bfc42bb
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-03-24 10:02:32 +0200

    :zap: add mainlayout to pages

--- frontend/src/pages/Pages.tsx
+++ frontend/src/pages/Pages.tsx
@@ -1,15 +1,21 @@
-import type { FC } from "react";
+import { type FC } from "react";
 import Login from "./Login/Login";
-import DarkModeButton from "../components/DarkModeButton/DarckModeButton";
+import DarkModeButton from "../components/DarkModeButton/DarkModeButton";
+import MainLayout from "../layouts/MainLayout/MainLayout";
+// import { AuthContext } from "../hooks/auth";
 
 interface PagesProps {}
 
 const Pages: FC<PagesProps> = () => {
+    // const {auth} = useContext(AuthContext)
     return (
-        <div className="text-light-colors-text dark:text-dark-colors-text">
-            <div className="h-screen w-screen  dark:bg-pr">
-                <Login />
-            </div>
+        <div className="transition ease-linear duration-500 text-light-colors-text dark:text-dark-colors-text bg-light-colors-dashboard-primary-bg dark:bg-dark-colors-dashboard-primary-bg">
+            <div className="h-screen w-screen">
+                <MainLayout>
+                </MainLayout>
+                {/* <Login/> */}
+            </div>  
+            
             <DarkModeButton/>
         </div>
     );
