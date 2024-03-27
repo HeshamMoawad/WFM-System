adfe347c4b6e675eb6519f91488089f2eaec015c
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-03-27 10:35:00 +0200

    :zap: add dashboard page with pages routing

---
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react';
+
+interface DashBoardProps {}
+
+const DashBoard: FC<DashBoardProps> = () => {
+    return (
+        <div className='bg-[red] h-screen w-screen'>
+            
+        </div>
+    );
+}
+
+export default DashBoard;
--- frontend/src/pages/Pages.tsx
+++ frontend/src/pages/Pages.tsx
@@ -1,24 +1,48 @@
 import { type FC } from "react";
-import Login from "./Login/Login";
-import DarkModeButton from "../components/DarkModeButton/DarkModeButton";
+import { BrowserRouter,Routes, Route} from 'react-router-dom';
+// import Login from "./Login/Login";
 import MainLayout from "../layouts/MainLayout/MainLayout";
-// import { AuthContext } from "../hooks/auth";
+// import {useAuth} from "../hooks/auth";
+// import DashBoard from "./Dashboard/Dashboard";
+import DarkModeButton from "../components/DarkModeButton/DarkModeButton";
+import DashBoard from "./Dashboard/Dashboard";
+import { useAuth } from "../hooks/auth";
+import Login from "./Login/Login";
 
 interface PagesProps {}
 
 const Pages: FC<PagesProps> = () => {
-    // const {auth} = useContext(AuthContext)
-    return (
-        <div className="transition ease-linear duration-500 text-light-colors-text dark:text-dark-colors-text bg-light-colors-dashboard-primary-bg dark:bg-dark-colors-dashboard-primary-bg">
-            <div className="h-screen w-screen">
-                <MainLayout>
-                </MainLayout>
-                {/* <Login/> */}
-            </div>  
-            
-            <DarkModeButton/>
-        </div>
-    );
+    const {auth} = useAuth({})
+    return (<>
+    {
+        auth.Authorization ? 
+        (
+            <div className="
+                overflow-x-hidden
+                transition 
+                ease-linear 
+                duration-500 
+                text-light-colors-text 
+                dark:text-dark-colors-text 
+                bg-light-colors-dashboard-primary-bg 
+                dark:bg-dark-colors-dashboard-primary-bg
+                ">
+                    <div className="h-[screen] w-screen">
+                        <MainLayout>
+                            <BrowserRouter>
+                                <Routes>
+                                    <Route path="/dashboard" element={<DashBoard/>}/>
+                                </Routes>
+                            </BrowserRouter>
+                        </MainLayout>
+                    </div>  
+                    <DarkModeButton/>
+            </div>
+        ):(
+            <Login/>
+        )
+    }
+    </>);
 };
 
 export default Pages;
