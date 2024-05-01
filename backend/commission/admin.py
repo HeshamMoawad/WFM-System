704eaac33bbed7047435fc611ffdb3200df18309
HeshamMoawad <heshammowad120120@yahoo.com>
2024-05-01 13:29:22 +0200

    :zap: new app Commission

---
+++ backend/commission/admin.py
@@ -0,0 +1,124 @@
+from django.contrib import admin
+from .models import Team , CoinChanger , DeductionRules , TargetSlice , UserCommissionDetails
+from utils.admin_utils import FieldSets
+
+# Register your models here.
+
+class DedactionRulesAdminSite(admin.ModelAdmin):
+    list_display = ["late_time","deduction_days" , "department" , "is_global"]
+    list_filter = ["department"]
+    readonly_fields = ['uuid',"created_at","updated_at"]
+    # search_fields = ['name', 'uuid']  
+    fieldsets = FieldSets([
+            'Deduction Fields' ,
+            'Other Fields'
+        ],[
+            [
+                'late_time',
+                'deduction_days',
+                'is_global',
+                'department',
+            ],[
+                "uuid" ,
+                "created_at",
+                "updated_at"
+            ]
+    ]).fieldsets
+
+
+
+class TargetSliceAdminSite(admin.ModelAdmin):
+    list_display = ["min_value","max_value", "money" , "is_global" , 'is_money_percentage' , 'department']
+    list_filter = ["is_global" , "department" , "is_money_percentage"]
+    readonly_fields = ['uuid',"created_at","updated_at"]
+    search_fields = ['min_value', 'max_value' , "money"]  
+    fieldsets = FieldSets([
+            'TargetSlice Fields' ,
+            'Other Fields'
+        ],[
+            [
+                'min_value',
+                'max_value',
+                'money',
+                'is_global',
+                'is_money_percentage',
+                'department',
+            ],[
+                "uuid" ,
+                "created_at",
+                "updated_at"
+            ]
+    ]).fieldsets
+ 
+class TeamAdminSite(admin.ModelAdmin):
+    list_display = ["name","leader"]
+    # list_filter = ["is_global" ]
+    readonly_fields = ['uuid',"created_at","updated_at"]
+    search_fields = ['name', 'leader__username' , "agents__username"]  
+    fieldsets = FieldSets([
+            'Team Fields' ,
+            'Other Fields'
+        ],[
+            [
+                'name',
+                'leader',
+                'agents',
+                'commission_rules',
+            ],[
+                "uuid" ,
+                "created_at",
+                "updated_at"
+            ]
+    ]).fieldsets
+
+
+class CoinChangerAdminSite(admin.ModelAdmin):
+    readonly_fields = ['uuid',"created_at","updated_at","date"]
+    fieldsets = FieldSets([
+            'CoinChanger Fields' ,
+            'Other Fields'
+        ],[
+            [
+                'sar',
+                'date',
+            ],[
+                "uuid" ,
+                "created_at",
+                "updated_at"
+            ]
+    ]).fieldsets
+
+
+class UserCommissionDetailsAdminSite(admin.ModelAdmin):
+    list_display = ["user","basic","set_deduction_rules","set_global_commission_rules" ,"will_arrive_at", "will_leave_at"]
+    list_filter = ["set_deduction_rules" , "set_global_commission_rules" ]
+    readonly_fields = ['uuid',"created_at","updated_at"]
+    search_fields = ['user__username']  
+    fieldsets = FieldSets([
+            'UserCommissionDetails Fields' ,
+            'Other Fields'
+        ],[
+            [
+                'user',
+                'basic',
+                'set_deduction_rules',
+                'set_global_commission_rules',
+                'deduction_rules',
+                'commission_rules',
+                'will_arrive_at',
+                'will_leave_at',
+
+            ],[
+                "uuid" ,
+                "created_at",
+                "updated_at"
+            ]
+    ]).fieldsets
+
+
+
+admin.site.register(Team , TeamAdminSite)
+admin.site.register(CoinChanger, CoinChangerAdminSite)
+admin.site.register(DeductionRules,DedactionRulesAdminSite)
+admin.site.register(TargetSlice , TargetSliceAdminSite)
+admin.site.register(UserCommissionDetails , UserCommissionDetailsAdminSite)
---
+++ backend/commission/models.py
@@ -0,0 +1,106 @@
+from django.db import models
+from django.db.models.signals import pre_save , post_save
+from users.models import  User ,Department , create_update_history
+from core.models import BaseModel
+import typing , datetime
+# Create your models here.
+
+
+class TargetSlice(BaseModel):
+    min_value = models.PositiveIntegerField(verbose_name="Min Value")
+    max_value = models.PositiveIntegerField(verbose_name="Max Value")
+    money = models.CharField(verbose_name="Commission", max_length=100)
+    is_money_percentage = models.BooleanField(verbose_name="Set Commession as Percentage")
+    is_global = models.BooleanField(verbose_name="Set Global Rule")
+    department = models.ForeignKey(Department, verbose_name="", on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Target Slice"
+        verbose_name_plural = "Target Slices"
+ 
+    def is_achieved(self,count:int):
+        return count > self.min_value
+        
+    def is_in_range(self,count:int):
+        return self.min_value < count < self.max_value
+
+    def __str__(self):
+        return f"{self.min_value} - {self.max_value} : {self.money} {'%' if self.is_money_percentage else 'EGP'}"
+
+
+
+class DeductionRules(BaseModel):
+    late_time = models.PositiveIntegerField(verbose_name="Late in Secounds")
+    deduction_days = models.FloatField(verbose_name="Deduction in Days")
+    is_global = models.BooleanField(verbose_name="Set Global Rule")
+    department = models.ForeignKey(Department, verbose_name="", on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Deduction Rules"
+        verbose_name_plural = "Deduction Rules"
+
+    def __str__(self) -> str:
+        return f"{self.late_time/60}min - {self.deduction_days} day - {self.department}"
+
+
+
+class UserCommissionDetails(BaseModel):
+    user = models.OneToOneField(User,verbose_name="User" , on_delete=models.CASCADE)
+    basic = models.PositiveIntegerField(verbose_name="Basic Salary"  , default= 0)
+    set_deduction_rules = models.BooleanField(verbose_name="Set Deduction Rules" , default=True)
+    deduction_rules = models.ManyToManyField(DeductionRules,verbose_name="Deduction Rules")
+    set_global_commission_rules = models.BooleanField(verbose_name="Set Global Rule" , default=True)
+    commission_rules = models.ManyToManyField(TargetSlice,verbose_name="Commission Target Slices")
+    will_arrive_at = models.TimeField(verbose_name="Must Arrive At",default=datetime.time(9,0,0))
+    will_leave_at = models.TimeField(verbose_name="Must Leave At",default=datetime.time(17,0,0))
+
+    def __str__(self):
+        return f"{self.user} - {self.set_global_commission_rules} - {self.set_deduction_rules}"
+
+    class Meta:
+        verbose_name = 'User Commission Details'
+        verbose_name_plural = 'User Commission Details'
+
+
+
+class Team(BaseModel):
+    name = models.CharField(verbose_name="Team Name", max_length=100)
+    leader =  models.ForeignKey(User,verbose_name="Leader" , related_name="team_leader", on_delete=models.SET_NULL , null=True)
+    agents = models.ManyToManyField(User,verbose_name="Agents" , related_name="team_agents")
+    commission_rules = models.ManyToManyField(TargetSlice,verbose_name="Commission Rules")
+    def __str__(self):
+        return self.name
+
+
+
+class CoinChanger(BaseModel):
+    egp_to_sar = models.FloatField(verbose_name="1 SAR equal to ? in EGP")
+    date  = models.DateField(verbose_name="Changer Date" , auto_now=True )
+    def __str__(self):
+        return self.date.strftime("%Y/%m")
+
+    class Meta:
+        verbose_name = 'Coin Changer'
+        verbose_name_plural = 'Coin Changers'    
+        unique_together = ['date',]
+
+    def calc_concurrency(self , sar_value:typing.Union[int,float]):
+        return sar_value * self.egp_to_sar
+        
+
+
+def create_user_commission_details(sender, instance:User, created ,**kwargs):
+    if created :
+        details = UserCommissionDetails.objects.create(
+            user=instance ,
+            basic=kwargs.get('basic',0),
+            set_deduction_rules=kwargs.get('set_deduction_rules',True),
+            set_global_commission_rules=kwargs.get('set_global_commission_rules',True),
+            )
+        details.save()
+
+
+pre_save.connect(create_update_history,sender=Team)
+pre_save.connect(create_update_history,sender=CoinChanger)
+pre_save.connect(create_update_history, sender=TargetSlice)
+pre_save.connect(create_update_history, sender=DeductionRules)
+pre_save.connect(create_update_history, sender=UserCommissionDetails)
+post_save.connect(create_user_commission_details,sender=User)
