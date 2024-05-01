50e1a625f157f879932892dcb72f3a7564096d49
HeshamMoawad <heshammowad120120@yahoo.com>
2024-05-01 13:08:19 +0200

    :zap: add models choices and move fieldsets

--- backend/users/types.py
+++ backend/users/types.py
@@ -4,3 +4,17 @@ class UserTypes (models.TextChoices):
     AGENT = ("AGENT", "Agent")
     MANAGER = ("MANAGER", "Manager")
     OWNER = ('OWNER','Owner')
+    HR = ('HR','HR')
+
+
+class RequestTypes (models.TextChoices):
+    GLOBAL = ("GLOBAL", "Global")
+    ANNUAL = ("ANNUAL", "Annual")
+    VACATION = ('VACATION','Vacation')
+    LATE = ('LATE','Late')
+
+
+class RequestStatuses (models.TextChoices):
+    PENDING = ("PENDING", "Pending")
+    ACCEPTED = ("ANNUAL", "Accepted")
+    REJECTED = ('VACATION','Rejected')
