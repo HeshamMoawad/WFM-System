71db40ffd099473267f1ed56ca08901e0bde6404
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-06-11 14:27:05 +0300

    :art: add method to be called when add rows in component

--- frontend/src/components/SelectComponent/SelectComponent.tsx
+++ frontend/src/components/SelectComponent/SelectComponent.tsx
@@ -1,6 +1,7 @@
 import {  ReactElement, SetStateAction, type FC } from 'react';
 import useRequest from '../../hooks/calls';
 import LoadingComponent from '../LoadingComponent/LoadingComponent';
+import { getArgsFrom } from '../../utils/converter';
 // import {Department} from '../../types/auth';
 
 interface SelectComponentProps {
@@ -13,15 +14,17 @@ interface SelectComponentProps {
     config:{
         value:string;
         label:string|string[];
+        method?:undefined|Function;
     }
     selected?:string[]
     setSelection?:React.Dispatch<SetStateAction<any>>;
     moreOptions?:{label:string , value:string}[]
     refresh?:boolean;
+    required?:boolean;
 }
 
 
-function SelectComponent<ValuesType>({name,LabelName,url,selectClassName , LabelClassName , refresh , multiple,setSelection ,moreOptions, config={value:"uuid",label:"name"} , selected=[] }:SelectComponentProps):ReactElement {
+function SelectComponent<ValuesType>({name,LabelName,url,selectClassName , LabelClassName , refresh , multiple,setSelection ,moreOptions, config={value:"uuid",label:"name" , method:undefined} , selected=[] , required=false }:SelectComponentProps):ReactElement {
     // const depend = refresh ? [refresh] : [];
     const {data , loading } = useRequest<ValuesType>({
         method:"GET",
@@ -33,7 +36,7 @@ function SelectComponent<ValuesType>({name,LabelName,url,selectClassName , Label
             loading ? <LoadingComponent/> : null
         }
         <label htmlFor={name} className={`${LabelClassName}`}>{LabelName}</label>
-        <select onChange={(e)=>setSelection?setSelection(e.currentTarget.value) : null} multiple={multiple} name={name} className={`${selectClassName}`}>
+        <select onChange={(e)=>setSelection?setSelection(e.currentTarget.value) : null} multiple={multiple} name={name} className={`${selectClassName}`} required={required}>
             {
                 moreOptions?.map((_,index) => {
                     return (
@@ -44,10 +47,13 @@ function SelectComponent<ValuesType>({name,LabelName,url,selectClassName , Label
             {
                 data?.results?.map((_,index) => {
                     const item = _ as any;
+                    const row_text = config.method ? 
+                        config.method(...getArgsFrom(item,config.label)) : 
+                        typeof config.label === "string"? item[config.label] : `${config.label.map((value) =>` ${item[value]} `)}`
+
                     return (
                         <option key={index} value={item[config.value]} selected={selected.includes(item[config.value])}>{
-                            typeof config.label === "string"? item[config.label] : `${config.label.map((value) =>` ${item[value]} `)}`
-                            
+                            row_text
                             }</option>
                     )
                 })
