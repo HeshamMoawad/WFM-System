3abb9ad5cf5601b1dac9a17032222c28694fe3b4
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-06-11 14:24:51 +0300

    :zap: create advanceform

---
+++ frontend/src/components/AdvanceForm/AdvanceForm.tsx
@@ -0,0 +1,69 @@
+import { useState, type FC } from 'react';
+import Container from '../../layouts/Container/Container';
+import { useAuth } from '../../hooks/auth';
+import SelectComponent from '../SelectComponent/SelectComponent';
+// import { TRANSLATIONS } from '../../utils/constants';
+import { sendRequest } from '../../calls/base';
+import { parseFormData } from '../../utils/converter';
+import LoadingComponent from '../LoadingComponent/LoadingComponent';
+import Swal from 'sweetalert2';
+
+interface AdvanceFormProps {}
+
+const AdvanceForm: FC<AdvanceFormProps> = () => {
+    const {auth} = useAuth()
+    const [loading , setLoading] = useState(false)
+    return (
+    <Container className='relative w-[40%] h-fit'>
+        {
+            loading ? <LoadingComponent/>:<></>
+        }
+        <form action="" method="post" className='flex flex-col gap-4' onSubmit={e=>{
+            e.preventDefault();
+            setLoading(true);
+            sendRequest({method: 'POST',url:"api/treasury/advance",data:parseFormData(e)})
+                .then(data=>{
+                        Swal.fire({
+                            position: "center",
+                            icon: "success",
+                            title: "Successfully",
+                            showConfirmButton: false,
+                            timer: 1000
+                        })
+                })
+                .catch(error=>{
+                        Swal.fire({
+                            position: "center",
+                            icon: "error",
+                            title: "Faild to Add",
+                            showConfirmButton: false,
+                            timer: 1000
+                        })
+            })
+                .finally(()=>setLoading(false));
+        }}>
+                <input type="hidden" name='user' value={auth.uuid}/>
+                <div className='flex flex-row justify-between'>
+                    <SelectComponent
+                            selectClassName=''
+                            LabelName='Taker'
+                            url='api/users/user'
+                            name='user'
+                            config={{
+                                value:"uuid",
+                                label:"username"
+                            }}
+                        />
+                </div>
+                <div className='flex flex-row justify-between'>
+                    <label htmlFor="amount">Amount</label>
+                    <input type="number" min={10} name="amount" id="amount" className='w-[100%] md:w-[50%]  place-self-center outline-none px-4 rounded-lg border border-[gray] bg-light-colors-login-third-bg dark:border-[#374558] dark:bg-dark-colors-login-third-bg' />
+                </div>
+                <button type='submit' className='bg-btns-colors-primary rounded-md mb-4 h-9'>Submit</button>
+            </form>
+
+    </Container>
+    );
+}
+
+export default AdvanceForm;
