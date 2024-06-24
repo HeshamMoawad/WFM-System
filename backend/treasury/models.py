ab293817b1fcc3d206db9023e49435e11b9d69cf
HeshamMoawad <heshammowad120120@yahoo.com>
2024-06-24 10:44:23 +0200

    :zap: add signal to create auto outcome records

--- backend/treasury/models.py
+++ backend/treasury/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save , post_save
 from core.models import  BaseModel 
 from users.models import User , create_update_history
 from django.utils.timezone import now , timedelta
@@ -22,6 +22,9 @@ class Advance(BaseModel):
 class TreasuryOutcome(BaseModel):
     creator = models.ForeignKey(User,verbose_name="Creator" ,  on_delete=models.SET_NULL , null=True , limit_choices_to={'role': 'OWNER'})#limit_choices_to={'role': 'OWNER'}
     amount = models.PositiveIntegerField(verbose_name="Amount")
+    from_advance =  models.ForeignKey(Advance,verbose_name="Advance" ,  on_delete=models.CASCADE , null=True , blank=True)
+    # from_salary =  models.ForeignKey(,verbose_name="Salary" ,  on_delete=models.CASCADE , null=True , blank=True)
+    # from_basic =  models.ForeignKey(,verbose_name="Basic" ,  on_delete=models.CASCADE , null=True , blank=True)
     details = models.CharField(verbose_name="Details" , max_length=250 )
 
 
@@ -33,6 +36,24 @@ class Notification(BaseModel):
     deadline = models.DateTimeField(verbose_name="Deadline",default=tomorrow)
 
 
+
+
+
+
+def add_treasury_record(sender:User, instance:User, created:bool, **kwargs):
+    if created :
+        new_record = TreasuryOutcome.objects.create(
+            creator = instance.creator,
+            amount = instance.amount ,
+            from_advance=instance,
+            details = f"Advance to {instance.user} amount {instance.amount} EGP" , 
+        )
+        new_record.save()
+        
+
 pre_save.connect(create_update_history, sender=TreasuryIncome)
 pre_save.connect(create_update_history, sender=TreasuryOutcome)
 pre_save.connect(create_update_history, sender=Advance)
+post_save.connect(add_treasury_record, sender=Advance)
+
+
