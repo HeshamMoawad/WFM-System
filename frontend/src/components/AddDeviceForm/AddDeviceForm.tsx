f0bdc58f24fb0d50460ca40f28f12f1a15d7ccd1
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-08-07 09:57:52 +0300

    :alien: add translastion with some params to endpoint call

--- frontend/src/components/AddDeviceForm/AddDeviceForm.tsx
+++ frontend/src/components/AddDeviceForm/AddDeviceForm.tsx
@@ -1,10 +1,13 @@
-import { useState, type FC, type FormEvent } from "react";
+import { useContext, useState, type FC, type FormEvent } from "react";
 import Container from "../../layouts/Container/Container";
 import SelectComponent from "../SelectComponent/SelectComponent";
 import { sendRequest } from "../../calls/base";
 import { parseFormData } from "../../utils/converter";
 import LoadingComponent from "../LoadingComponent/LoadingComponent";
 import Swal from "sweetalert2";
+import { useAuth } from "../../hooks/auth";
+import { LanguageContext } from "../../contexts/LanguageContext";
+import { TRANSLATIONS } from "../../utils/constants";
 
 interface AddDevicesFormProps {
     refresh:number,
@@ -13,7 +16,11 @@ interface AddDevicesFormProps {
 }
 
 const AddDevicesForm: FC<AddDevicesFormProps> = ({refresh , setRefresh}) => {
+    const {lang} = useContext(LanguageContext)
     const [loading , setLoading] = useState(false)
+    const {auth} = useAuth()
+    const additionalFilter = auth.role === "OWNER" || auth.is_superuser ? {} : {department__name : auth.department.name}
+
     const onSubmit = (e:FormEvent<HTMLFormElement>)=>{
         e.preventDefault();
         sendRequest({url : "api/users/device-access" , method:"POST" , data:parseFormData(e) })
@@ -47,29 +54,32 @@ const AddDevicesForm: FC<AddDevicesFormProps> = ({refresh , setRefresh}) => {
                 loading? <LoadingComponent/> : <></>
             }
             <label className="text-center text-xl w-full block">
-                Add Device
+                {TRANSLATIONS.DeviceAccess.form.title[lang]}
             </label>
-            <form className="grid grid-cols-3 space-y-3 items-center mb-4 " onSubmit={onSubmit} >
+            <form className="grid grid-cols-3  space-y-3 items-center mb-4 " onSubmit={onSubmit} >
                 <SelectComponent
-                    selectClassName="col-span-2 place-self-center"
+                    selectClassName="w-11/12 place-self-center"
+                    searchClassName="w-2/3"
                     LabelClassName="text-xl col-span-1"
-                    LabelName="User"
+                    LabelName={TRANSLATIONS.DeviceAccess.form.user[lang]}
                     url="api/users/user"
                     name="user"
                     config={{
                         value: "uuid",
                         label: "username",
                     }}
+                    params={additionalFilter}
                     required={true}
+                    searchOptions={{attr_name:"username__contains"}}
                 />
                 
-                <label htmlFor="name" className="col-span-1">Device Name</label>
+                <label htmlFor="name" className="col-span-1">{TRANSLATIONS.DeviceAccess.form.name[lang]}</label>
                 <input type="text" className="col-span-2" name="name" id="name" placeholder="Name" required/>
                 
-                <label htmlFor="unique_id" className="col-span-1">Device ID</label>
+                <label htmlFor="unique_id" className="col-span-1">{TRANSLATIONS.DeviceAccess.form.id[lang]}</label>
                 <input type="text" name="unique_id" className="col-span-2" id="unique_id" placeholder="ID" required/>
                 
-                <button type="submit" className="bg-btns-colors-primary rounded-md mb-4 h-9 col-span-3">Add</button>
+                <button type="submit" className="bg-btns-colors-primary rounded-md mb-4 h-9 col-span-3">{TRANSLATIONS.DeviceAccess.form.add[lang]}</button>
 
             </form>
         </Container>
