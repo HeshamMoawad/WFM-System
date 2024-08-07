ec9d57e3769871aa1471c0333818592dbb06bf1a
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-08-07 10:00:22 +0300

    :hammer: make container more interactive

--- frontend/src/layouts/Container/Container.tsx
+++ frontend/src/layouts/Container/Container.tsx
@@ -1,13 +1,21 @@
 import type { FC } from 'react';
 import { ChildrenType } from '../../types/base';
 
-interface ContainerProps extends ChildrenType{
-    className?:string
+interface ContainerProps extends ChildrenType {
+    className?:string;
+    disabled?:boolean;
+    onClick?:()=>void;
+    onDoubleClick?:()=>void;
 }
 
-const Container: FC<ContainerProps> = ({children , className}:ContainerProps) => {
+const Container: FC<ContainerProps> = ({children , className , onClick , onDoubleClick , disabled = false}:ContainerProps) => {
     return (
-        <div className={`container max-w-[93vw] transition-all duration-500 rounded-lg shadow-md m-2 px-3 pt-2 hover:mt-0 bg-light-colors-dashboard-secondry-bg dark:bg-dark-colors-dashboard-secondry-bg overflow-auto ${className}`}>
+        <div 
+        onDoubleClick={onDoubleClick}
+        onClick={onClick}
+        className={`container max-w-[93vw] transition-all duration-600 rounded-lg shadow-md m-2 px-3 pt-2 hover:mt-0 bg-light-colors-dashboard-secondry-bg dark:bg-dark-colors-dashboard-secondry-bg overflow-auto ${className}`}
+        aria-disabled={disabled}
+        >
             {children}
         </div>
     );
