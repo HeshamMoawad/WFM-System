69c8cbc05304ebd1ad7e1bfbb0949b3026a50d50
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-11-10 15:24:17 +0200

    :zap: update logic of calc target to be more usable

--- frontend/src/utils/calculator.ts
+++ frontend/src/utils/calculator.ts
@@ -1,6 +1,16 @@
 import { TargetSlice } from "../types/auth"
 
 
+const isIntger = (string_int:string)=>{
+    const parsed = parseInt(string_int)
+    return !isNaN(parsed) && parsed.toString() === string_int.trim()
+}
+
+const isFloat = (string_float:string)=>{
+    const parsed = parseFloat(string_float)
+    return !isNaN(parsed) && parsed.toString() === string_float.trim()
+}
+
 
 const calc_target_from_array = (count:number , targets:TargetSlice[] , percentageCost:number=1)=>{
     let result = 0
@@ -9,19 +19,25 @@ const calc_target_from_array = (count:number , targets:TargetSlice[] , percentag
         if (element.min_value <= count && count <= element.max_value){
             if(element.is_money_percentage){
                 result = (+element.money/100)*count * percentageCost
-            } else {
+            } 
+            else if (isIntger(element.money) || isFloat(element.money)) {
                 result = +element.money;
+            }
+            else {
+                result = eval(element.money.replaceAll("$var",String(count)))
             }
             break;
         }else if (index === targets.length - 1 && count >= element.max_value){
             if(element.is_money_percentage){
                 result = (+element.money/100)*count * percentageCost
-            } else {
+            } 
+            else if (isIntger(element.money) || isFloat(element.money)) {
                 result = +element.money;
             }
+            else {
+                result = eval(element.money.replaceAll("$var",String(count)))
+            }
         }
-        
-        
     }
     return Math.round(result);
 }
