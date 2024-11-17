ddc514082e4acffc3712f2349e4d884ef095692a
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-11-17 08:28:51 +0200

    :zap: empty databases

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -101,25 +101,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'wfmsystem',
-#         'USER': 'wfm',
-#         'PASSWORD': 'wfmpassword',
-#         'HOST': '192.168.11.235',  # Set to 'localhost' if running locally
-#         'PORT': '5432',  # Set to '5432' for default
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
