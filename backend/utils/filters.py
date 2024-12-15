36858131802ce1f0c0d7c402ae139185ada51451
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-12-15 09:58:02 +0200

    :bug: fix generic filters conflects

--- backend/utils/filters.py
+++ backend/utils/filters.py
@@ -12,7 +12,8 @@ def filter_queryset_with_permissions(user:User,queryset:QuerySet,model:Model)->Q
     }
     if not user.is_superuser:
         cont_type = ContentType.objects.get_for_model(model)
-        filters_text_list = GenericFilter.objects.filter(groups__in=user.custom_groups.values_list("uuid"),content_type=cont_type).values_list("get_queryset_text",flat=True) 
+        filters_text_list = GenericFilter.objects.filter(groups__in=user.custom_groups.values_list("uuid"),content_type=cont_type).order_by('layer_index').values_list("get_queryset_text",flat=True) 
+        filters_text_list = [f"\n{f}\n" for f in filters_text_list ]
         if filters_text_list :
             exec("".join(filters_text_list),{},context)
         else :
